35b5508f913c25b82d7965b95e3e34a86ea2168e
yakkwinjiwon <yakkwinjiwon@gmail.com>
2024-08-02 17:25:09 +0900

    init

---
+++ src/main/java/com/pigeon_stargram/sns_clone/SnsCloneApplication.java
@@ -0,0 +1,13 @@
+package com.pigeon_stargram.sns_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pigeon_stargram/sns_clone/SnsCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigeon_stargram.sns_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
