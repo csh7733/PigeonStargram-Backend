5a7b40ec29c77c98bd45d35ecdc450abebad2742
yakkwinjiwon <yakkwinjiwon@gmail.com>
2024-08-02 17:52:12 +0900

    config-init

---
+++ src/main/java/com/pigeon_stargram/sns_clone/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.pigeon_stargram.sns_clone.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
