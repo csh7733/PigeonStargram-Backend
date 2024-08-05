2dd1589ae83638088daa538661656942fe884aea
최성현 <popopqo777@gmail.com>
2024-08-06 00:39:39 +0900

    refactor : TestData 수정

--- src/main/java/com/pigeon_stargram/sns_clone/TestData.java
+++ src/main/java/com/pigeon_stargram/sns_clone/TestData.java
@@ -30,45 +30,55 @@ public class TestData {
 
     @PostConstruct
     public void initData() {
+        // Creating Users
         User johnDoe = new User("John Doe", "img-user.png");
+        User janeSmith = new User("Jane Smith", "avatar-2.png");
+        User aliceBrown = new User("Alice Brown", "avatar-3.png");
+
         userRepository.save(johnDoe);
+        userRepository.save(janeSmith);
+        userRepository.save(aliceBrown);
 
-        // Post 1
-        Posts post1 = postsService.createPost(johnDoe, "켄터22키");
+        // Post 1 by John Doe
+        Posts post1 = postsService.createPost(johnDoe, "Hello from John Doe!");
+        Comment post1_comment1 = commentService.createComment(janeSmith, post1, "Hi John!");
+        replyService.createReply(aliceBrown, post1_comment1, "Hello Jane!");
 
-        Comment post1_comment1 = commentService.createComment(johnDoe, post1, "Test");
-
-        // Post 2
+        // Post 2 by Jane Smith
         List<Image> images2 = List.of(new Image("img-profile1.png", true));
-        Posts post2 = postsService.createPost(johnDoe, "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. There are many variations of passages.", images2);
+        Posts post2 = postsService.createPost(janeSmith, "Jane's beautiful day at the park.", images2);
+        Comment post2_comment1 = commentService.createComment(johnDoe, post2, "Looks great, Jane!");
+        replyService.createReply(aliceBrown, post2_comment1, "Amazing picture!");
+        Comment post2_comment2 = commentService.createComment(aliceBrown, post2, "Love the scenery.");
 
-        Comment post2_comment1 = commentService.createComment(johnDoe, post2, "Test");
-        replyService.createReply(johnDoe, post2_comment1, "Test Reply");
-        replyService.createReply(johnDoe, post2_comment1, "Demo");
-
-        Comment post2_comment2 = commentService.createComment(johnDoe, post2, "It is a long established fact that a reader will be distracted by the readable content of a page.");
-        Comment post2_comment3 = commentService.createComment(johnDoe, post2, "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.There are many variations of passages.");
-        replyService.createReply(johnDoe, post2_comment3, "It is a long established fact that a reader will be distracted by the readable content.");
-
-        // Post 3
+        // Post 3 by Alice Brown
         List<Image> images3 = List.of(new Image("img-profile2.jpg", true), new Image("img-profile3.jpg", true));
-        Posts post3 = postsService.createPost(johnDoe, "It is a long established fact that a reader will be distracted by the readable content of a page", images3);
+        Posts post3 = postsService.createPost(aliceBrown, "Alice's adventure in the mountains.", images3);
+        Comment post3_comment1 = commentService.createComment(johnDoe, post3, "Wow, awesome view!");
+        replyService.createReply(janeSmith, post3_comment1, "I agree, it's stunning!");
+        Comment post3_comment2 = commentService.createComment(janeSmith, post3, "Wish I was there!");
 
-        Comment post3_comment1 = commentService.createComment(johnDoe, post3, "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.");
-        replyService.createReply(johnDoe, post3_comment1, "Test");
-        replyService.createReply(johnDoe, post3_comment1, "Test To Reply");
-        replyService.createReply(johnDoe, post3_comment1, "test");
+        // Post 4 by John Doe
+        Posts post4 = postsService.createPost(johnDoe, "Back to work after a great vacation.");
+        Comment post4_comment1 = commentService.createComment(aliceBrown, post4, "Hope you had a good time!");
+        replyService.createReply(janeSmith, post4_comment1, "Welcome back!");
 
-        // Post 4
-//        Posts post4 = postsService.createPost(johnDoe, "It is a long established fact that a reader will be distracted by the readable content of a page");
-        // Assuming there's a way to set video (which isn't shown in the Posts entity above)
-        // post4.setVideo("vyJU9efvUtQ");
+        // Logging Posts
+        List<PostsDto> postsByJohnDoe = postsService.getPostsByUser(johnDoe);
+        List<PostsDto> postsByJaneSmith = postsService.getPostsByUser(janeSmith);
+        List<PostsDto> postsByAliceBrown = postsService.getPostsByUser(aliceBrown);
 
-        List<PostsDto> postsByUser = postsService.getPostsByUser(johnDoe);
-        postsByUser.forEach(post -> {
+        logPosts(postsByJohnDoe);
+        logPosts(postsByJaneSmith);
+        logPosts(postsByAliceBrown);
+
+        log.info("Print finish");
+    }
+
+    private void logPosts(List<PostsDto> posts) {
+        posts.forEach(post -> {
             String jsonString = jsonUtil.toJson(post);
             log.info(jsonString);
         });
-        log.info("Print finish");
     }
 }
