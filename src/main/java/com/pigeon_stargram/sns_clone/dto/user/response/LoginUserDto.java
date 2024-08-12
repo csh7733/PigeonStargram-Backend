e795ce153714199f133c92968f961e2dd1d54ffd
최성현 <popopqo777@gmail.com>
2024-08-12 17:41:59 +0900

    refactor : 현재 유저 정보 가져올때 리턴하는 필드 추가

--- src/main/java/com/pigeon_stargram/sns_clone/dto/user/response/LoginUserDto.java
+++ src/main/java/com/pigeon_stargram/sns_clone/dto/user/response/LoginUserDto.java
@@ -1,5 +1,6 @@
 package com.pigeon_stargram.sns_clone.dto.user.response;
 
+import com.pigeon_stargram.sns_clone.domain.user.User;
 import lombok.*;
 @ToString
 @Getter
@@ -8,10 +9,16 @@ import lombok.*;
 @AllArgsConstructor
 public class LoginUserDto {
     private Boolean isLoggedIn;
-    private Long user;
+    private Long userId;
+    private String name;
+    private String company;
+    private String avatar;
 
-    public LoginUserDto(Long userId) {
+    public LoginUserDto(User user) {
         isLoggedIn = true;
-        this.user = userId;
+        this.userId = user.getId();
+        this.name = user.getName();
+        this.company = user.getCompany();
+        this.avatar = user.getAvatar();
     }
 }
