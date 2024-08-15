6c07c6d20d3cbf79636c27485f0db4da74c40322
최성현 <popopqo777@gmail.com>
2024-08-15 18:14:19 +0900

    refactor : 게시글,댓글,답글 write작업 이후 전체 게시물 -> 특정 유저의 최신 게시글만 보이도록 수정

--- src/main/java/com/pigeon_stargram/sns_clone/dto/reply/request/RequestDeleteReplyDto.java
+++ src/main/java/com/pigeon_stargram/sns_clone/dto/reply/request/RequestDeleteReplyDto.java
@@ -1,7 +1,5 @@
 package com.pigeon_stargram.sns_clone.dto.reply.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.transaction.Transactional;
 import lombok.*;
 
 @Getter
@@ -9,8 +7,6 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
-public class RequestLikeReplyDto {
+public class RequestDeleteReplyDto {
     private Long postUserId;
-    private Long replyId;
 }
